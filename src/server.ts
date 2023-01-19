974f1179d90044302cacdb885620e25c47046c4f
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-01-18 23:13:19 -0300

    :tada: First Commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify"
+import cors from '@fastify/cors'
+import { prisma } from './lib/prisma'
+import { appRoutes } from "./routes"
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+  port: 3333
+}).then(()=> {
+  console.log('HTTP server running on port 3333')
+})
